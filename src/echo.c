1f827c43472a1f18745bffe56266448989ce526f
dct2012 <cameron.taaffe@gmail.com>
2015-08-11 19:27:25 -0500

    added newline argument to echo

--- src/echo.c
+++ src/echo.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
-    for(int i = 1; i < argc; i++)
-        printf("%s ", argv[i]);
-    putchar('\n');
+    int newline = 0;
+
+    if(argc > 1 && (strcmp(argv[1], "-n") == 0))
+        newline = 1;
+
+    for(int i = 1 + newline; i < argc; i++) {
+        fputs(argv[i], stdout);
+        if(i + 1 != argc)
+            putchar(' ');
+    }
+    
+    if(!newline)
+        putchar('\n');
 
     return 0;
 }
